22357dbab27ede151b1e50d476a1edfd5cc1b792
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-07-23 08:49:16 +0200

    // Version updated to v0.10.0

--- blockadvertising.php
+++ blockadvertising.php
@@ -45,7 +45,7 @@ class BlockAdvertising extends Module
 	{
 		$this->name = 'blockadvertising';
 		$this->tab = 'advertising_marketing';
-		$this->version = '0.9.3';
+		$this->version = '0.10.0';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
